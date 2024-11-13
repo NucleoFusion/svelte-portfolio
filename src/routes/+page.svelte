c66d1cf6ce9d1be50306ebb49b25ea0582fc2a8d
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-11-13 12:23:11 +0530

    Adding Home and partial responsiveness to navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,45 @@
 <script>
   import Navbar from "./navbar.svelte";
+
+  import linkedin from "$lib/assets/linkedin.png";
+  import leetcode from "$lib/assets/leetcode.png";
+  import resume from "$lib/assets/resume.png";
+  import github from "$lib/assets/github.png";
+  import { fly } from "svelte/transition";
+  import { onMount } from "svelte";
+
+  let mount = false;
+  onMount(() => {
+    mount = true;
+  });
+
+  import resumepdf from "$lib/assets/lakshitresume.pdf";
 </script>
 
-<Navbar />
+{#if mount}
+  <Navbar />
+  <div class="home-container jetBrainsMono">
+    <div class="name" in:fly={{ x: -200, duration: 850 }}>
+      <h1>Lakshit Singh</h1>
+    </div>
+    <div class="about" in:fly={{ x: 200, duration: 850 }}>
+      A software developer specializing in Back-End and Systems Development from
+      Delhi, India.
+    </div>
+    <div class="icons" in:fly={{ x: -200, duration: 850 }}>
+      <a href={resumepdf} download><img src={resume} alt="resume" /></a>
+      <a href="https://github.com/NucleoFusion"
+        ><img src={github} alt="github" /></a
+      >
+      <a href="https://www.linkedin.com/in/lakshit-singh-41583a287/"
+        ><img src={linkedin} alt="linkedin" /></a
+      >
+      <a href="https://leetcode.com/u/NucleoFusion/"
+        ><img src={leetcode} alt="leetcode" /></a
+      >
+    </div>
+  </div>
+{/if}
 
 <style>
   @import "./home.css";
@@ -22,5 +59,18 @@
     font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
+  }
+
+  :global(.bg) {
+    background-color: black;
+    opacity: 0;
+    z-index: -10;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
   }
 </style>
--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,54 +1,186 @@
 <script>
   import { redirect } from "@sveltejs/kit";
-
+  import { fly } from "svelte/transition";
   import src from "$lib/assets/home.png";
+  import dd from "$lib/assets/dd.png";
 
   function onclick(url) {
     redirect(302, url);
   }
+
+  let width = $state(window.innerWidth);
+  window.onresize = function () {
+    width = window.innerWidth;
+  };
+
+  let toggleDD = () => {
+    console.log(document.querySelector("#dd").style.visibility);
+    if (document.querySelector("#dd").style.visibility === "visible") {
+      document.querySelector("#dd").style.visibility = "hidden";
+      document.querySelector(".bg").style.opacity = "0";
+      document.querySelector(".bg").style.zIndex = -10;
+      return;
+    }
+    document.querySelector("#dd").style.visibility = "visible";
+    document.querySelector(".bg").style.opacity = "0.6";
+    document.querySelector(".bg").style.zIndex = 10;
+  };
+
+  $effect(() => {
+    console.log($state.snapshot(width));
+  });
 </script>
 
-<div class="container jetBrainsMono">
-  <div>
+{#if width > 700}
+  <div class="container jetBrainsMono" in:fly={{ y: -200, duration: 700 }}>
     <div>
       <a href="/">
         <img alt="home" {src} />
       </a>
     </div>
+    <div>
+      <div
+        role="button"
+        tabindex="0"
+        class="button-style"
+        onclick={() => {
+          onclick("/tui");
+        }}
+        onkeydown={() => {
+          onclick("/tui");
+        }}
+      >
+        TUI
+      </div>
+      <div
+        role="button"
+        tabindex="0"
+        class="button-style"
+        onclick={() => {
+          onclick("/projects");
+        }}
+        onkeydown={() => {
+          onclick("/projects");
+        }}
+      >
+        Projects
+      </div>
+      <div
+        role="button"
+        tabindex="0"
+        class="button-style"
+        onclick={() => {
+          onclick("/about");
+        }}
+        onkeydown={() => {
+          onclick("/about");
+        }}
+      >
+        About
+      </div>
+      <div
+        role="button"
+        tabindex="0"
+        class="button-style"
+        onclick={() => {
+          onclick("/contact");
+        }}
+        onkeydown={() => {
+          onclick("/contact");
+        }}
+      >
+        Contact
+      </div>
+    </div>
   </div>
-  <div>
-    <div
-      class="button-style"
-      onclick={() => {
-        onclick("/projects");
-      }}
-    >
-      <a href="/projects" aria-disabled="true">Projects</a>
+{:else}
+  <div class="dd-container jetBrainsMono" in:fly={{ y: -200, duration: 700 }}>
+    <div>
+      <a href="/">
+        <img alt="home" {src} />
+      </a>
     </div>
-    <div
-      class="button-style"
-      onclick={() => {
-        onclick("/projects");
-      }}
-    >
-      <a href="/projects" aria-disabled="true">About</a>
+    <div class="dropdown" id="dd">
+      <div>
+        <div
+          role="button"
+          tabindex="0"
+          class="button-style"
+          onclick={() => {
+            onclick("/tui");
+          }}
+          onkeydown={() => {
+            onclick("/tui");
+          }}
+        >
+          TUI
+        </div>
+      </div>
+      <div>
+        <div
+          role="button"
+          tabindex="0"
+          class="button-style"
+          onclick={() => {
+            onclick("/projects");
+          }}
+          onkeydown={() => {
+            onclick("/projects");
+          }}
+        >
+          Projects
+        </div>
+      </div>
+      <div>
+        <div
+          role="button"
+          tabindex="0"
+          class="button-style"
+          onclick={() => {
+            onclick("/about");
+          }}
+          onkeydown={() => {
+            onclick("/about");
+          }}
+        >
+          About
+        </div>
+      </div>
+      <div>
+        <div
+          role="button"
+          tabindex="0"
+          class="button-style"
+          onclick={() => {
+            onclick("/contact");
+          }}
+          onkeydown={() => {
+            onclick("/contact");
+          }}
+        >
+          Contact
+        </div>
+      </div>
     </div>
-    <div
-      class="button-style"
-      onclick={() => {
-        onclick("/contact");
-      }}
-    >
-      <a href="/contact" aria-disabled="true">Contact</a>
+    <div>
+      <div
+        class="button-style"
+        role="button"
+        tabindex="0"
+        onclick={toggleDD}
+        onkeydown={toggleDD}
+      >
+        <img alt="dropdown" src={dd} />
+      </div>
     </div>
   </div>
-</div>
+{/if}
 
 <style>
   .container {
     background-color: var(--secondary);
 
-    width: 94vw;
+    width: 90vw;
     height: 10vh;
 
     margin-top: 3vh;
@@ -67,11 +199,98 @@
     grid-template-columns: 1fr 1fr;
 
     padding: 0vh 1vw;
+
+    z-index: 11;
+  }
+
+  .dd-container {
+    background-color: var(--secondary);
+
+    width: 90vw;
+    height: 10vh;
+
+    margin-top: 3vh;
+    margin-left: 3vw;
+
+    border-radius: 20px;
+
+    box-shadow:
+      5px 5px 20px rgb(0, 0, 0, 0.3),
+      -5px -5px 20px rgb(0, 0, 0, 0.3);
+
+    border: solid var(--bg-color) 2px;
+
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+
+    padding: 0vh 1vw;
+
+    position: relative;
+
+    z-index: 11;
+  }
+
+  .dd-container > div {
+    display: grid;
+    align-items: center;
+
+    padding: 0 2vw;
+  }
+
+  .dd-container > div:last-child {
+    justify-content: right;
+  }
+
+  .dd-container > div:last-child > .button-style {
+    width: 9vw;
+    border: none;
+  }
+
+  .dd-container > div:last-child img {
+    width: 30px;
+    height: 30px;
+  }
+
+  .dropdown {
+    width: 90vw;
+    height: 30vh;
+
+    visibility: hidden;
+    position: absolute;
+
+    background-color: var(--secondary);
+    bottom: -32vh;
+    left: 0px;
+
+    border-radius: 20px;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+
+    z-index: 11;
+  }
+
+  .dropdown > div {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .dropdown > div > div {
+    width: 30vw;
   }
 
   .container > div:last-child {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-template-rows: 1fr;
 
     justify-content: center;
@@ -115,8 +334,21 @@
     transform: scale(0.95);
   }
 
-  .button-style a {
-    color: inherit;
-    text-decoration: none;
+  @media (max-width: 1050px) {
+    .container {
+      grid-template-columns: 30vw 60vw;
+    }
+    .button-style {
+      width: 10vw;
+    }
+  }
+
+  @media (max-width: 800px) {
+    .container {
+      grid-template-columns: 20vw 70vw;
+    }
+    .button-style {
+      width: 13vw;
+    }
   }
 </style>
