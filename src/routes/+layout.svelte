0b725bde7b1018bb5c0eafdbd9e808133c6d292f
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-11-14 22:48:00 +0530

    Adding ABout Page Skeleton and Experience Data

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Navbar from "./navbar.svelte";
+
+  import { onMount } from "svelte";
+
+  let mount = $state(false);
+  onMount(() => (mount = true));
+
+  let { children } = $props();
+</script>
+
+{#if mount}
+  <Navbar />
+{/if}
+
+{@render children()}
+
+<style>
+  /* Global Styles */
+  :global(body) {
+    background-color: #303446;
+  }
+
+  :global(:root) {
+    --bg-color: #303446;
+    --secondary: #b7bdf8;
+  }
+
+  :global(.jetBrainsMono) {
+    font-family: "JetBrains Mono", monospace;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  :global(.bg) {
+    background-color: black;
+    opacity: 0;
+    z-index: -10;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+
+  let mount = $state(false);
+  onMount(() => (mount = true));
+</script>
+
+{#if mount}
+  <div class="main-container jetBrainsMono">
+    <div class="descr">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas amet
+      vero, harum qui id adipisci dicta suscipit, odio et nisi labore a
+      accusantium facere eum aspernatur expedita veritatis, quis nam.
+    </div>
+    <div class="experience">
+      <h2>Experience</h2>
+      <div class="experience-div">
+        <div>
+          <h3>
+            Saas Implementation and API Intgrations for CRM - Courtyard Farms
+          </h3>
+          <h3>May - July, 2024</h3>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          soluta, vel repellat atque explicabo nulla laboriosam odit a dolorem
+          totam! Soluta dolores reiciendis numquam assumenda aliquam eveniet
+          doloremque deserunt asperiores!
+        </p>
+      </div>
+    </div>
+    <div>Skills</div>
+    <div>Education</div>
+  </div>
+{/if}
+
+<style>
+  @import "./about.css";
+</style>
