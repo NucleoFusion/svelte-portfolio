f4dd85271ddb6ba775f6ad2c977986817bc9f319
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-11-19 20:49:34 +0530

    Adding outline of projects

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+  let projects = [
+    "transpohub",
+    "footballAPI",
+    "shortIt",
+    "e-comm",
+    "lapisOAuth",
+  ];
+
+  let curr = $state(1);
+  let prev = $derived(curr - 1);
+  let next = $derived(curr + 1);
+
+  function increment() {
+    if (curr < 4) {
+      curr += 1;
+    }
+  }
+  function decrement() {
+    if (curr > 0) {
+      curr -= 1;
+    }
+  }
+</script>
+
+<div class="projects-container jetBrainsMono">
+  <div class="floater">
+    <button onclick={decrement}>{"<"}</button>
+    <h1>{curr}</h1>
+    <button onclick={increment}>{">"}</button>
+  </div>
+  {#if prev > 0}
+    <div>{prev}</div>
+  {/if}
+  {#if curr < 4}
+    <div>{next}</div>
+  {/if}
+</div>
+
+<style>
+  @import "./projects.css";
+</style>
